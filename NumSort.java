6794c8455c8182430133f9cfb45f8b8e4e669d0c
KyaBetke <betkek@limestone.on.ca>
2023-05-25 09:28:58 -0400

    testing

--- NumSort.java
+++ NumSort.java
@@ -2,35 +2,49 @@ import java.util.Scanner;
 
 public class NumSort {
     public static void main(String[] args) {
-        //
+        int counter = 0;
         Scanner sc = new Scanner(System.in);
         int gameSize = Integer.parseInt(sc.nextLine());
-        int square = gameSize * gameSize;
-        System.out.print("\033[H\033[2J"); // clear screen
-        int [][] array = {{1,2,3,4,5}, {6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24}};
         
-        for (int k = 0; k < square; k++)
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    if (array[i][j] == k) {
-                        createFilledArray(gameSize, k);
-                        System.out.print(k + "  ");
+        int square = gameSize * gameSize;
+       
+        System.out.print("\033[H\033[2J"); // clear screen
+
+        int [] oneDArray = new int [5];
+        printArray(oneDArray);
+        
+    //     int [][] array = new int [gameSize][gameSize];
+        
+    //     for (int k = 0; k < square; k++)
+    //         for (int i = 0; i < array.length; i++) {
+    //             for (int j = 0; j < array[i].length; j++) {
+    //                 if (array[i][j] == k) {
+    //                     createFilledArray(gameSize, k);
+    //                     System.out.print(k + "  ");
                    
-                    }
+    //                 }
 
-                }
+    //             }
                 
-            }
+    //         }
 
 
+    // }
+    // public static int[] createFilledArray(int size, int value) {
+    //     int[] array = new int[size];
+
+    //     for (int i = 0; i < size; i++) {
+    //         array[i] = value;
+    //     }
+
+    //     return array;
     }
-    public static int[] createFilledArray(int size, int value) {
-        int[] array = new int[size];
 
-        for (int i = 0; i < size; i++) {
-            array[i] = value;
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+          System.out.println(array[i]);
+            System.out.print(", ");
         }
-
-        return array;
     }
+
 }
