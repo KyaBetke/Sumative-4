f07700f6dacf53669cc36f9ee4fd67c0adebbb72
KyaBetke <betkek@limestone.on.ca>
2023-05-26 08:24:47 -0400

    Prints random grid with empty space for 0

---
+++ SwapPuzzle.java
@@ -0,0 +1,141 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class SwapPuzzle {
+
+
+    public static void main(String[] args) {
+        //
+        System.out.print("\033[H\033[2J"); // clear screen
+        Scanner sc = new Scanner(System.in);
+        int gameSize = Integer.parseInt(sc.nextLine());
+        System.out.println();
+        int[][] createArray = createBoard(gameSize);
+        // int square = gameSize * gameSize;
+
+
+        printBoard(createArray);
+        /*
+         * loop until done
+         * find allowed numbers
+         * ask user to chose
+         * swap numbers
+         * check if done
+         * if done, exit somehow
+         * else print board
+         * repeat
+         */
+
+
+    }
+
+
+    public static int[][] createBoard(int dimension) {
+        int count = 0;
+        int[] twoDArray = loadRandomNumbers(dimension * dimension);
+        int[][] board = new int[dimension][dimension];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = twoDArray[count];
+                count++;
+            }
+        }
+        return board;
+        // - load the 2 dimenional array
+    }
+
+
+    public static int[] loadRandomNumbers(int square) {
+        int[] randomArray = new int[square];
+        Random rand = new Random();
+        for (int i = 0; i < square; i++) {
+            randomArray[i] = i;
+
+
+        }
+
+
+        for (int i = 0; i < randomArray.length; i++) {
+            int randSwap = rand.nextInt(randomArray.length - 1);
+            int temp = randomArray[randSwap];
+            randomArray[randSwap] = randomArray[i];
+            randomArray[i] = temp;
+        }
+        // System.out.println(Arrays.toString(randomArray));
+        return randomArray;
+        // - load the single dimension array by swaping numbers
+        // - called by createBoard
+    }
+
+
+    public static void printBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+
+                if (board[i][j] == 0) {
+                    System.out.printf(" %3s ", " ");
+                } else {
+                    System.out.printf(" %3s ",board[i][j]);
+                }
+
+
+            }
+            System.out.println("\n");
+        }
+    }
+
+
+    public static int[] findPosition(int number, int[][] board) {
+        boolean breakLoop = false;
+        int[] coorodinates = new int[2];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    coorodinates = new int[] { i, j };
+                    breakLoop = true;
+                    break;
+                }
+
+
+            }
+            if (breakLoop == true) {
+                break;
+            }
+        }
+
+
+        return coorodinates;
+    }
+
+
+    // public static void swapPosition(int number, int[][] board) {
+    // // int[] coordinates = findPosition(0, board);
+    // }
+
+
+    // public static int[] findAllowedNumbers(int[][] board, int dimension) {
+    // int[] coordinates = findPosition(0, board);
+    // // - find the position of 0 and use it to find all the other numbers the user
+    // is
+    // // allowed to select
+    // }
+
+
+    // public static boolean isValidSelection(int[] allowedNumber, int
+    // selectedNumber) {
+    // // - validation method to determine if the user has selected an allowed
+    // number)
+    // }
+
+
+    // public static boolean isComplete(int[][]board);
+
+
+}
+
+
+
+
+
